06be9d919a121f579eea7f3a296fac01951e0dbc
newerk <juniusowens@yahoo.com>
2024-03-21 17:04:26 -0400

    Create Empty .pug Files for Controller Modules to Use Later

--- controllers/corporationController.js
+++ controllers/corporationController.js
@@ -1,10 +1,21 @@
 const asyncHandler = require("express-async-handler");
+const Corporation = require("../models/coporation");
 
 module.exports = {
   placeholder: asyncHandler(async (req, res, next) => {
     //placeholder
     res.render("index", {
       title: "Welcome to The Parts Shop 621",
+    });
+  }),
+
+  //get all corporations
+  corp_list: asyncHandler(async (req, res, next) => {
+    const allCorps = await Corporation.find().sort({ name: 1 }).exec();
+
+    res.render("corp_list", {
+      title: "Corporations",
+      corporations: allCorps,
     });
   }),
 };
